2eaf3e5be9554d1eb80cb74f05411b9d78b0a7d6
helenserdiuk <76928777+helenserdiuk@users.noreply.github.com>
2022-06-03 03:28:49 +0000

    Deploying to gh-pages from @ helenserdiuk/goit-js-hw-09@300cdbff6d33c052bb6c857f9dc2cc7e7edcf05c 🚀

--- 01-color-switcher.75595e01.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null,o=document.body;t.addEventListener("click",(function(){n=setInterval((function(){t.disabled=!0,e.disabled=!1,o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.75595e01.js.map
